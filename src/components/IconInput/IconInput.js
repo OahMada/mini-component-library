c8dca4329438567ceae6f728e010edc28119044b
Adam Hao <haozg44@outlook.com>
2025-03-01 00:38:03 +0800

    apply some improvements after watching solution video

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -8,94 +8,84 @@ import VisuallyHidden from '../VisuallyHidden';
 
 var STYLES = {
 	small: {
-		iconSize: 18,
-		iconStrokeWIdth: 1,
+		iconSize: 16,
+		strokeWidth: 1,
 		fontSize: 14,
 		verticalPadding: 4,
 		leftPadding: 24,
 	},
 	large: {
 		iconSize: 24,
-		iconStrokeWIdth: 2,
+		strokeWidth: 2,
 		fontSize: 18,
 		verticalPadding: 8,
 		leftPadding: 36,
 	},
 };
 
-const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
+const IconInput = ({ label, icon, width = 250, size, ...delegated }) => {
 	let styles = STYLES[size];
 
 	return (
-		<Wrapper style={{ '--width': width + 'px' }}>
-			<VisuallyHidden>
-				<label htmlFor='search'>{label}</label>
-			</VisuallyHidden>
+		<Wrapper>
+			<VisuallyHidden>{label}</VisuallyHidden>
 			<SearchInput
 				type='text'
-				id='search'
-				placeholder={placeholder}
 				style={{
-					'--fontSize': styles.fontSize + 'px',
-					'--verticalPadding': styles.verticalPadding + 'px',
-					'--leftPadding': styles.leftPadding + 'px',
+					'--width': width + 'px',
+					'--font-size': styles.fontSize / 16 + 'rem',
+					'--vertical-padding': styles.verticalPadding + 'px',
+					'--left-padding': styles.leftPadding + 'px',
+					'--border-thickness': styles.strokeWidth + 'px',
 				}}
+				{...delegated}
 			/>
 			<IconWrapper style={{ '--size': styles.iconSize + 'px' }}>
-				<Icon id={icon} size={styles.iconSize} strokeWidth={styles.iconStrokeWIdth} />
+				<Icon id={icon} size={styles.iconSize} strokeWidth={styles.strokeWidth} />
 			</IconWrapper>
 		</Wrapper>
 	);
 };
 
-var Wrapper = styled.div`
-	width: var(--width);
-	display: flex;
+var Wrapper = styled.label`
 	position: relative;
-`;
-
-var IconWrapper = styled.div`
-	color: ${COLORS.gray700};
-	position: absolute;
-	left: 0;
-
-	/* for the icon to be a little bit shifted up */
-	top: -2px;
-	bottom: 0;
-	margin: auto;
-	width: var(--size);
-	height: var(--size);
-
-	// To make clicking the icon also focus the input.
-	pointer-events: none;
-`;
-
-var SearchInput = styled.input`
 	display: block;
-	width: 100%;
-	border: none;
-	border-bottom: 1px solid ${COLORS.black};
-	font-size: calc(var(--fontSize) / 16 * 1rem);
-	font-weight: 700;
 	color: ${COLORS.gray700};
-	padding: var(--verticalPadding) 0;
-	padding-left: var(--leftPadding);
-
-	&::placeholder {
-		color: ${COLORS.gray500};
-		font-weight: 400;
-	}
-
-	&:focus {
-		outline-offset: 2px;
-	}
 
 	&:hover {
 		color: ${COLORS.black};
 	}
+`;
 
-	&:hover + ${IconWrapper} {
-		color: ${COLORS.black};
+var IconWrapper = styled.div`
+	position: absolute;
+	/* left: 0; */
+
+	/* for the icon to be a little bit shifted up */
+	top: -2px;
+	bottom: 0;
+	margin: auto 0;
+	/* width: var(--size); */
+	height: var(--size);
+
+	// To make clicking the icon also focus the input.
+	/* pointer-events: none; */
+`;
+
+var SearchInput = styled.input`
+	width: var(--width);
+	font-size: var(--font-size);
+	border: none;
+	border-bottom: var(--border-thickness) solid ${COLORS.black};
+	font-weight: 700;
+	color: inherit;
+	padding: var(--vertical-padding) 0;
+	padding-left: var(--left-padding);
+	outline-offset: 2px;
+
+	&::placeholder {
+		color: ${COLORS.gray500};
+		font-weight: 400;
 	}
 `;
 
